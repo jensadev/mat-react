1fc2a1e4d821f3c5ca7f418f99b0599e26357952
jensnti <jens.andreasson@ga.ntig.se>
2021-03-15 17:15:45 +0100

    refactor(mealslist): edit/delete buttons

--- src/components/MealsList.js
+++ src/components/MealsList.js
@@ -2,12 +2,21 @@ import './MealsList.scss';
 
 // import ScheduleIcon from '@material-ui/icons/ScheduleRounded';
 import {
+  // DeleteOutlineRounded,
+  DeleteRounded,
+  EditRounded,
   FastfoodRounded,
   FreeBreakfastRounded,
   RestaurantMenuRounded
 } from '@material-ui/icons';
+import { Field, Form } from 'react-final-form';
 
 import Date from './Date';
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const onSubmit = async (values) => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
 
 function ListItem(props) {
   const TypeIcon = (type) => {
@@ -25,12 +34,48 @@ function ListItem(props) {
 
   const meal = props.meal;
   return (
-    <div className="d-flex pt-3 text-dark border-bottom">
-      <div className="mealicon">{TypeIcon(meal.typeId)}</div>
-      <p className="pb-3 mb-0 lh-sm text-dark">
-        <Date classes={'d-block small text-muted'} dateString={meal.date} />
-        {meal.dish}
-      </p>
+    <div className="d-flex justify-content-between pt-3 text-dark border-bottom">
+      <div className="d-flex">
+        <div className="mealicon">{TypeIcon(meal.typeId)}</div>
+
+        <p className="pb-3 mb-0 lh-sm text-dark">
+          <Date classes={'d-block small text-muted'} dateString={meal.date} />
+          {meal.dish}
+        </p>
+      </div>
+      <div>
+        <Form
+          onSubmit={onSubmit}
+          render={({ handleSubmit, form, submitting }) => (
+            <form onSubmit={handleSubmit}>
+              <Field
+                type="hidden"
+                name="id"
+                component="input"
+                defaultValue={meal.id}
+              />
+              <button
+                type="submit"
+                className="btn btn-outline-dark border-0"
+                disabled={submitting}
+                onClick={() => {
+                  form.change('action', 'edit');
+                }}>
+                <EditRounded />
+              </button>
+              <button
+                type="submit"
+                className="btn btn-outline-dark border-0"
+                disabled={submitting}
+                onClick={() => {
+                  form.change('action', 'delete');
+                }}>
+                <DeleteRounded />
+              </button>
+            </form>
+          )}
+        />
+      </div>
     </div>
   );
 }
