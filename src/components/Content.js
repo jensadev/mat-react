863a5c4d06eab526742bd662752e34ec6082717c
jensnti <jens.andreasson@ga.ntig.se>
2021-03-25 20:50:42 +0100

    fix: img

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 
+import Fisk from '../images/fisk.svg';
+
 class Content extends Component {
   render() {
     return (
       <main className="d-flex flex-column container mt-3">
         <div className="row">
-          <div className="col-md-5 d-flex flex-column justify-content-center">
-            <h1 className="display-4">Fluid jumbotron</h1>
-            <p className="lead">
-              This is a modified jumbotron that occupies the entire horizontal
-              space of its parent.
-            </p>
-          </div>
-          <div className="col-md-7">
+          <div className="order-md-1 col-md-7">
             <figure className="figure w-100">
-              <img
-                src="/images/fisk.svg"
-                alt="fisk"
-                className="figure-img img-fluid"
-              />
+              <img src={Fisk} alt="fisk" className="figure-img img-fluid" />
               <figcaption className="figure-caption text-end">
                 <a
                   href="https://iconscout.com/illustrations/cooking-food"
@@ -35,6 +26,10 @@ class Content extends Component {
                 </a>
               </figcaption>
             </figure>
+          </div>
+          <div className="col-md-5 d-flex flex-column justify-content-center">
+            <h1 className="display-4">Mat mat mat</h1>
+            <p className="lead">Äta bör en annars dör hen...</p>
           </div>
         </div>
       </main>
